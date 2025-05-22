5e1ec95b9fb9228d59a70dacc62528a45fad041e
Rafael Germano <rrgermano@gmail.com>
2025-05-22 20:36:21 -0300

    add credit button

--- outflows/templatetags/tags.py
+++ outflows/templatetags/tags.py
@@ -1,5 +1,4 @@
 from django import template
-from django.apps import apps
 
 register = template.Library()
 
@@ -9,14 +8,8 @@ def has_attr(obj, name):
 
 @register.filter
 def has_field(queryset, field_name):
-    """
-    Verifica se o modelo de um queryset tem um campo específico
-    Uso: {% if outflows|model_has_field:"paid" %}
-    """
     if not queryset:
         return False
-    
-    # Pega o modelo do primeiro objeto
     model = queryset.model if hasattr(queryset, 'model') else queryset[0].__class__
     
     try:
