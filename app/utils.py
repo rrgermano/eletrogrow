343bf03a02890bf23bbb944db09a990027275c67
Rafael <rrgermano@gmail.com>
2025-05-25 19:09:10 -0300

    add cmd import_clients

--- app/utils.py
+++ app/utils.py
@@ -8,12 +8,12 @@ def is_date_util(date, delta="Positive"):
         date = date.date()
     except:
         pass
-    calendario = mcal.get_calendar("BMF")
-    dias_uteis = calendario.schedule(start_date=datetime.datetime.strftime(date - relativedelta(days=7),
+    calendar = mcal.get_calendar("BMF")
+    util_days = calendar.schedule(start_date=datetime.datetime.strftime(date - relativedelta(days=7),
                                                                            '%Y-%m-%d'),
                                      end_date=datetime.datetime.strftime(date + relativedelta(days=7),
                                                                          '%Y-%m-%d'))
-    while datetime.datetime.fromtimestamp(time.mktime(date.timetuple())) not in dias_uteis.index:
+    while datetime.datetime.fromtimestamp(time.mktime(date.timetuple())) not in util_days.index:
         if delta.capitalize() == "Positive":
             date = date + relativedelta(days=1)
         else:
@@ -21,30 +21,27 @@ def is_date_util(date, delta="Positive"):
     return date
 
 
-def calcular_periodo_cartao(tipo, mes_vigente):
-    # Se não for especificado, usa o mês atual
+def credit_period(requested_month=None):
+    if not requested_month:
+        requested_month = datetime.date.today()
 
-    today = mes_vigente
+    today = requested_month
 
     if (today + relativedelta(days=7)) <= is_date_util(today.replace(day=3) + relativedelta(months=1)):
-        mes_vigente = today
+        requested_month = today
+    elif today.day > today.replace(day=3):
+        requested_month = today+relativedelta(months=1)
     else:
-        mes_vigente = today+relativedelta(months=1)
-    if tipo.lower() == 'inicio':
-        return is_date_util(mes_vigente.replace(day=3)) - relativedelta(days=7)
-    elif tipo.lower() == 'fim':
-        return is_date_util((mes_vigente+relativedelta(months=1)).replace(day=3)) - relativedelta(days=7)
-    else:
-        raise ValueError("Tipo inválido. Use 'inicio' ou 'fim'")
+        requested_month = today
 
-
-# Função adicional para obter o período completo
-def obter_periodo_completo(mes_vigente = None):
-    if not mes_vigente:
-        mes_vigente = datetime.date.today()
 
     return {
-        'fechamento': calcular_periodo_cartao('inicio', mes_vigente),
-        'vencimento': calcular_periodo_cartao('fim', mes_vigente),
-        'mes_referencia': mes_vigente.strftime('%B/%Y')
-    }
+        'initial': (
+                    is_date_util(requested_month.replace(day=3)) - relativedelta(days=7),
+                    requested_month.strftime('%B/%Y')
+                    ),
+        'closing': (
+                    is_date_util((requested_month+relativedelta(months=1)).replace(day=3)) - relativedelta(days=7),
+                    requested_month.strftime('%B/%Y')
+                    )
+            }
---
+++ clients/management/commands/import_clients.py
@@ -0,0 +1,9 @@
+from notion.notion_func import get_clients
+from clients.models import ModelClient
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        for client in get_clients():
+            ModelClient.objects.create(**client)
